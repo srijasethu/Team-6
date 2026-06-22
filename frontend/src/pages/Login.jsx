e5d9e81430c0804bfc5f3e595186b9407ef78346
srijasethu <sethusrija@gmail.com>
2026-06-22 12:35:36 +0530

    Initial React setup and project structure
    Created Vite React project, folder structure, Login page, Employee Dashboard, and Manager Dashboard files.

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <h1>Employee Leave Management System</h1>
+      <p>Login Page</p>
+    </div>
+  );
+}
+
+export default Login;
